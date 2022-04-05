46b1122eeee8fc0498a65a7c0c156f959faa2d5f
Carlos Antonio <carlitosablanco@gmail.com>
2022-04-05 00:45:34 +0000

    Haciendo problema 2

--- p2,3,4/p2.cpp
+++ p2,3,4/p2.cpp
@@ -3,9 +3,9 @@
 void p2_efficiency(float miles, float gallons)
 {
 	float efficiency = miles/gallons;
-	float l_every_hundred_km =
+	float l_every_hundred_km = gallons*3.785;
 	std::cout <<"efficiency is "<<efficiency<<"mi/gal, equal to "
-	<<l_every_hundred_km<<"l /km\n";;
+	<<l_every_hundred_km<<"l /km\n";
 }
 
 
