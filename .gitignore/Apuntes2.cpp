5672840e857011dab3911f9682c05de548714cfa
Carlos Antonio <carlitosablanco@gmail.com>
2022-04-12 20:17:16 +0000

    adding gitignore

---
+++ .gitignore/Apuntes2.cpp
@@ -0,0 +1,8 @@
+#include <iosteam>
+#include "Vector.h"
+
+
+
+
+
+
---
+++ .gitignore/Vector.h
@@ -0,0 +1,14 @@
+class Vector
+{
+private:
+
+public:
+
+}
+
+
+
+
+
+//.h is for just the things that the class have, BUT 
+//just their names, not their implementation, on .cpp  put "#include Vector.h"
